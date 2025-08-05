d77ba7b7ccbdb0c71290972e1cbafcfa939f8105
Chris Fauries <chrisfauries@gmail.com>
2025-08-05 16:21:40 -0500

    added oboe and bassoon

--- src/data/instruments/cardAccessor.ts
+++ src/data/instruments/cardAccessor.ts
@@ -1,10 +1,12 @@
 import { CLARINET_CARD_MAP } from "./clarinet";
 import { EUPHONIUM_CARD_MAP } from "./euphonium";
 import { FLUTE_CARD_MAP } from "./flute";
+import { OBOE_CARD_MAP } from "./oboe";
 import { HORN_CARD_MAP } from "./horn";
 import { INSTRUMENT, INSTRUMENT_CARD_LEVEL_MAP } from "./instrument";
 import { LEVEL } from "./level";
 import { SAXOPHONE_CARD_MAP } from "./saxophone";
+import { BASSOON_CARD_MAP } from "./bassoon";
 import { TROMBONE_CARD_MAP } from "./trombone";
 import { TRUMPET_CARD_MAP } from "./trumpet";
 import { TUBA_CARD_MAP } from "./tuba";
@@ -13,10 +15,14 @@ const getCardMap = (instrument: INSTRUMENT): INSTRUMENT_CARD_LEVEL_MAP => {
   switch (instrument) {
     case INSTRUMENT.FLUTE:
       return FLUTE_CARD_MAP;
+    case INSTRUMENT.OBOE:
+      return OBOE_CARD_MAP;
     case INSTRUMENT.CLARINET:
       return CLARINET_CARD_MAP;
     case INSTRUMENT.SAXOPHONE:
       return SAXOPHONE_CARD_MAP;
+    case INSTRUMENT.BASSOON:
+      return BASSOON_CARD_MAP;
     case INSTRUMENT.TRUMPET:
       return TRUMPET_CARD_MAP;
     case INSTRUMENT.HORN:
